5f41bbf6f67360b5b628b336fb11dce0d3402493
DilmurodZK <dilmurodzk@gmail.com>
2022-04-05 22:48:05 +0500

    2nd

--- dzcrm/settings.py
+++ dzcrm/settings.py
@@ -43,7 +43,7 @@ ROOT_URLCONF = 'dzcrm.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [ BASE_DIR / "frontend"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
